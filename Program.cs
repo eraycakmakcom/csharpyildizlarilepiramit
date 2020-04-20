fc123456ca6dae4164fed31fde164d5853d96a13
eraycakmakcom <eraycakmakblog@gmail.com>
2020-04-20 14:02:51 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace yildiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int yil = 1; yil < 20; yil += 2)
+            {
+                for (int diz = 0; diz < 9 - yil / 2; diz++)
+                    Console.Write(" ");
+
+                for (int diz = 0; diz < yil; diz++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+
+           
+
+            Console.ReadKey();
+        }
+    }
+}
